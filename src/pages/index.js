1597718eb9a08c13ea4b2ed7e75ab487a34d384b
UraFIZ <kratser88@gmail.com>
2019-03-08 17:21:34 +0200

    finished working on navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { FaBeer } from "react-icons/fa"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import styled from "styled-components"
 
 const IndexPage = () => (
   <Layout>
